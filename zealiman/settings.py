61eac9d9f73c6d87bef0cee67a6623f6c544572b
Zebi lobognon <zebilobognon@gmail.com>
2026-08-14 16:42:24 +0000

    fin

--- zealiman/settings.py
+++ zealiman/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'notqf&s%7lwij1@d2nsxp#3pmf2ehxi+a3u1_5w6!-ws3-qyq9'
+
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 # SECURITY WARNING: don't run with debug turned on in production!
